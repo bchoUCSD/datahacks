b01bb9cdb1f166cdb01c21f54843c5cee10df2ef
bchoUCSD <bcho@ucsd.edu>
2024-04-06 15:01:53 -0700

    react app :0000

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,29 @@
+import React from 'react';
+
 function App() {
-  
-  function handleClick(){
-    console.log("button clicked")
+  const [counter, setCounter] = React.useState(0)
+  const [input, setInput] = React.useState("")
+
+  function incrementClick(){
+    setCounter(prev => {return prev + 1})
   }
-  
-  
+
+  function decrementClick(){
+    setCounter(prev => {return prev - 1})
+  }
+
   
   return (
     <div className="App">
       <header className="App-header">
         <h1>This is our front end of the app</h1>
-        <button onClick={handleClick}>CLICK ME </button>
+        <button onClick={incrementClick}>Increment</button>
+        <button onClick={decrementClick}>Decrement</button>
+        <h1>{counter}</h1>
+        <input onChange={e => setInput(e.target.value)}></input>
+        <h1>User Types:{input}</h1>
+
+
       </header>
     </div>
   );
