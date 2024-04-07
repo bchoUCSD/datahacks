563f4cf57d4d764d80cd5a3b456a29cad25d9592
PeterNguyen4 <peternguyen644@gmail.com>
2024-04-07 10:35:34 -0700

    Rename variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import hourglass from './images/hourglass.png'
 
 function App() {
   const [plot, setPlot] = React.useState('')
@@ -15,7 +16,7 @@ function App() {
   };
 
   async function handlePost() {
-    if(selectedOption){
+    if (selectedOption) {
       try{
         axios.defaults.withCredentials = true;
         const res = await axios.post('http://127.0.0.1:8000/generate_plot', {
@@ -37,22 +38,21 @@ function App() {
   return (
     <div className="App">
       <section className="flex-container">
-          <Parallax pages={2.5} class="animation">
-            <ParallaxLayer offset={0} className="animation_layer parallax"></ParallaxLayer>
+          <Parallax pages={2.5} class="wrapper">
             <ParallaxLayer speed={.05} offset={0} style={{ height: "auto" }}>
-                <div className="animation_layer parallax" id="back-4"></div>
+                <div className="panel parallax" id="back-4"></div>
             </ParallaxLayer>
             <ParallaxLayer speed={.1} offset={0}style={{ height: "auto" }}>
-                <div className="animation_layer parallax" id="mid-3"></div>
+                <div className="panel parallax" id="mid-3"></div>
             </ParallaxLayer>
-            <ParallaxLayer speed={.2} offset={0}style={{ height: "auto" }}>
-                <div className="animation_layer parallax" id="mid-2"></div>
+            <ParallaxLayer speed={.3} offset={0} style={{ height: "auto" }}>
+                <div className="panel parallax" id="mid-2"></div>
             </ParallaxLayer>
-            <ParallaxLayer speed={.3} offset={0} sticky={{ start: 0, end: .2 }} className="title">
+            <ParallaxLayer speed={.3} offset={0} sticky={{ start: 0, end: .2 }} className="title" style={{ zIndex: 10 }}>
               <h1 className="heading">T-Race</h1>
             </ParallaxLayer>
-            <ParallaxLayer speed={.4} offset={0.2} style={{ height: "auto" }}>
-                  <div className="animation_layer parallax" id="front-1"></div>
+            <ParallaxLayer speed={.4} offset={0.15} style={{ height: "auto" }}>
+                  <div className="panel parallax" id="front-1"></div>
             </ParallaxLayer>
             <ParallaxLayer speed={.4} offset={1} factor={2}>
               <div className='visualizer'> 
@@ -60,17 +60,18 @@ function App() {
                   <h2 className='prompt'>Visualize how dinosaurs changed over time.</h2>
                   
                   <div className='menu'>
-                    <DropdownButton id="dropdown-basic-button" title={selectedOption? selectedOption:'Choose one' }>
-                      <Dropdown.Item onClick={() => handleOptionSelect('Early Jurassic')}>Early Jurassic</Dropdown.Item>
-                      <Dropdown.Item onClick={() => handleOptionSelect('Mid Jurassic')}>Mid Jurassic</Dropdown.Item>
-                      <Dropdown.Item onClick={() => handleOptionSelect('Late Jurassic')}>Late Jurassic</Dropdown.Item>
-                      <Dropdown.Item onClick={() => handleOptionSelect('Early Cretaceous')}>Early Cretaceous</Dropdown.Item>
-                      <Dropdown.Item onClick={() => handleOptionSelect('Late Cretaceous')}>Late Cretaceous</Dropdown.Item>
-                      <Dropdown.Item onClick={() => handleOptionSelect('Late Triassic')}>Late Triassic</Dropdown.Item>
-                    </DropdownButton>
-
+                    <div className='menu-dropdown'>
+                      <img src={hourglass} alt="Era" class="hourglass"></img>
+                      <DropdownButton id="dropdown-basic-button" title={selectedOption? selectedOption:'Choose one' }>
+                        <Dropdown.Item onClick={() => handleOptionSelect('Early Jurassic')}>Early Jurassic</Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleOptionSelect('Mid Jurassic')}>Mid Jurassic</Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleOptionSelect('Late Jurassic')}>Late Jurassic</Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleOptionSelect('Early Cretaceous')}>Early Cretaceous</Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleOptionSelect('Late Cretaceous')}>Late Cretaceous</Dropdown.Item>
+                        <Dropdown.Item onClick={() => handleOptionSelect('Late Triassic')}>Late Triassic</Dropdown.Item>
+                      </DropdownButton>
+                    </div>
                     <button class="btn btn-success" onClick={handlePost}>Start Race</button>
-                    
                   </div>
                   {plot && <img src={`data:image/png;base64,${plot}`} alt="Plot" />}
                 </div> 
