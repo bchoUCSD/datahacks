f6b7b17b4fb0e69f9fc26a6e666ad1096b8f1dda
PeterNguyen4 <peternguyen644@gmail.com>
2024-04-07 07:48:12 -0700

    Fix image sizing mismatch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,24 +39,24 @@ function App() {
       <section className="flex-container">
           <Parallax pages={2} class="animation">
             <ParallaxLayer offset={0} className="animation_layer parallax"></ParallaxLayer>
-            <ParallaxLayer speed={.05} offset={0}>
+            <ParallaxLayer speed={.05} offset={0} style={{ height: "auto" }}>
                 <div className="animation_layer parallax" id="back-4"></div>
             </ParallaxLayer>
-            <ParallaxLayer speed={.1} offset={0}>
+            <ParallaxLayer speed={.1} offset={0}style={{ height: "auto" }}>
                 <div className="animation_layer parallax" id="mid-3"></div>
             </ParallaxLayer>
-            <ParallaxLayer speed={.2} offset={0}>
+            <ParallaxLayer speed={.2} offset={0}style={{ height: "auto" }}>
                 <div className="animation_layer parallax" id="mid-2"></div>
             </ParallaxLayer>
             <ParallaxLayer speed={.3} offset={0} sticky={{ start: 0, end: .2 }} className="title">
               <h1 className="heading">T-Race</h1>
             </ParallaxLayer>
-            <ParallaxLayer speed={.4} offset={0}>
+            <ParallaxLayer speed={.4} offset={0.15} style={{ height: "auto" }}>
                 <div className='img-container'>
-                  <div className="animation_layer parallax" id="front-1" height='100%'></div>
+                  <div className="animation_layer parallax" id="front-1"></div>
                 </div>
             </ParallaxLayer>
-            <ParallaxLayer speed={.5} offset={1} factor={3}>
+            <ParallaxLayer speed={.4} offset={1} style={{ height: "auto" }}>
               <div className='visualizer'> 
                 <div className='vis-container'>
                   <h2 className='prompt'>Visualize how dinosaurs changed over time.</h2>
