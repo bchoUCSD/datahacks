c70725d1f6ae7d04a304c984f1ba26a41b29d48f
bchoUCSD <bcho@ucsd.edu>
2024-04-07 09:40:29 -0700

    Cleaned file structure

--- backend/cleaning.py
+++ backend/cleaning.py
@@ -3,12 +3,6 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data.csv')
-df
-
-df = df.dropna() # dropping NaN rows
-df
-
 def get_period_name (period_str):
     period_list = period_str.split() # splitting by white space and gets list of strings
     period_name = period_list[0] + " " + period_list[1]
@@ -37,18 +31,21 @@ def str_to_num (length_str):
     str_list = length_str.replace('m','')
     return float(str_list)
 
-names = df.get('period').apply(get_period_name)
-to_year = df.get('period').apply(get_period_to)
-from_year = df.get('period').apply(get_period_from)
-int_len = df.get('length').apply(str_to_num)
+def getDF():
+    df = pd.read_csv('data.csv')
+    df = df.dropna() # dropping NaN rows
 
-cleaned_df = df.assign(period_name = names, to_years_ago = to_year, from_years_ago = from_year, len_as_int = int_len)
+    names = df.get('period').apply(get_period_name)
+    to_year = df.get('period').apply(get_period_to)
+    from_year = df.get('period').apply(get_period_from)
+    int_len = df.get('length').apply(str_to_num)
 
-cleaned_df = cleaned_df.dropna()
-cleaned_df
+    cleaned_df = df.assign(period_name = names, to_years_ago = to_year, from_years_ago = from_year, len_as_int = int_len)
 
-#get_period_to("Early Jurassic 199-189 million years ago")
-cleaned_df.get("period_name").unique() # number of different period names
+    cleaned_df = cleaned_df.dropna()
 
-dino_length_df = cleaned_df.groupby(['period_name', "length"]).count().get(['name'])
-dino_length_df
+    #get_period_to("Early Jurassic 199-189 million years ago")
+    cleaned_df.get("period_name").unique() # number of different period names
+    return cleaned_df 
+    # dino_length_df = cleaned_df.groupby(['period_name', "length"]).count().get(['name'])
+    # return dino_length_df
